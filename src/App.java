5acd7ce3cb97b911b401248fadca53b5767106ad
arturgonzaga320 <arturgonzaga320@gmail.com>
2023-03-28 18:41:38 -0300

    img folder

--- src/App.java
+++ src/App.java
@@ -11,7 +11,7 @@ public class App
     public static void main(String[] args) throws Exception 
     {
 		// Protocolo: conexão HTTPS e buscar 250 filmes
-		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
 		URI endereco = URI.create(url);
 
 		HttpClient client = HttpClient.newHttpClient();
@@ -19,7 +19,6 @@ public class App
 		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 
 		String body = response.body();
-		System.out.println(body);
 	
 		// Extrair/Parciar dados desejados (titulo, poster, classificação)
 		JsonParser parser = new JsonParser();
@@ -27,11 +26,16 @@ public class App
 		//System.out.println(lista_filmes.size());
 		
 		// Exibir e manipular os dados
+		System.out.println("\u001b[37m");
 		for (Map<String,String> filme : lista_filmes)
 		{
-			System.out.println(filme.get("title"));
-			System.out.println(filme.get("image"));
-			System.out.println(filme.get("imDbRating"));
+			System.out.print("\u001b[33m"+ filme.get("title")+ "\u001b[m");
+
+			String n_stars = filme.get("imDbRating");
+			char a = n_stars[0];
+
+
+			System.out.println("\u001b[34m" + filme.get("image") + "\u001b[m");
 			System.out.println();
 		}
     }
