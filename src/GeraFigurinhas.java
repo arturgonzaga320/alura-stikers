ac96a396e3b3a7b13b8155fa87d0966a0184dae6
Artur Gonzaga <arturgonzaga320@gmail.com>
2023-03-28 21:32:10 +0000

    Update

---
+++ src/GeraFigurinhas.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class GeraFigurinhas
+{
+    public void cria()
+    {
+        // Ler imagem
+        ImageIO.read(new File());
+
+        // Cria nova imagem em memória com transparência e com tamanho novo
+
+        // Copiar a imagem original para nova imagem (em memória)
+
+        // Escrever frase em nova imagem
+
+        // Escrever 
+    }
+}
